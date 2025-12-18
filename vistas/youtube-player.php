b8a2eb842bb73022c246bbcef06451d5210d1e48
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-12-18 12:00:43 -0500

    Update youtube-player.php

--- vistas/youtube-player.php
+++ vistas/youtube-player.php
@@ -1,8 +1,12 @@
 <!-- YouTube player (derecha) -->
 <div class="video-wrapper">
     <div class="ratio ratio-16x9 h-75 w-100 m-0" style="">
-        <iframe id="videoPlayList" 
-            src="https://www.youtube.com/embed/brZEANdIKMU?list=PLy0Q2cGnTqFu0FolcBCIeQI9aJK3EFVeT&index=3&enablejsapi=1"
-            allowfullscreen></iframe>
+        <iframe
+            id="videoPlayList"
+            src="https://www.youtube.com/embed/brZEANdIKMU?list=PLy0Q2cGnTqFu0FolcBCIeQI9aJK3EFVeT&index=3&enablejsapi=1&autoplay=1&mute=1&playsinline=1"
+            allow="autoplay; encrypted-media" 
+            allowfullscreen >
+        </iframe>
+
     </div>
 </div>
